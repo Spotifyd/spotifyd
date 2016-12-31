cfdc3f9a0df0954eb8d914615e90110aa3af0c75
Simon Persson <simon@flaskpost.org>
2016-12-31 16:03:24 +0100

    Initial effort.

---
+++ src/config.rs
@@ -0,0 +1,75 @@
+use std::error::Error;
+use std::path::PathBuf;
+use std::convert::From;
+
+use librespot::session::{Bitrate, Config as SessionConfig};
+use librespot::cache::{NoCache, Cache, DefaultCache};
+use librespot::version;
+
+use xdg;
+use ini::Ini;
+
+pub struct SpotifydConfig {
+    pub log_path: Option<PathBuf>,
+    pub username: Option<String>,
+    pub password: Option<String>,
+    pub cache: Box<Cache + Send + Sync>,
+    pub backend: Option<String>,
+    pub session_config: SessionConfig,
+}
+
+fn get_config_file() -> Result<PathBuf, Box<Error>> {
+    let xdg_dirs = try!(xdg::BaseDirectories::with_prefix("spotifyd"));
+    xdg_dirs.find_config_file("spotifyd")
+        .ok_or(From::from("Couldn't find config file in XDG config directory."))
+}
+
+pub fn read_config() -> Result<SpotifydConfig, Box<Error>> {
+    let conf_path = try!(get_config_file());
+    let conf = try!(Ini::load_from_file(conf_path.to_str().unwrap()));
+
+    let global = conf.section(Some("global".to_owned()));
+    let spotifyd = conf.section(Some("spotifyd".to_owned()));
+
+    let lookup = |field| spotifyd.and_then(|s| s.get(field)).or(global.and_then(|s| s.get(field)));
+
+    let bitrate = spotifyd.and_then(|s| s.get("bitrate"))
+        .and_then(|b| match b.trim() {
+            "96" => Some(Bitrate::Bitrate96),
+            "160" => Some(Bitrate::Bitrate160),
+            "320" => Some(Bitrate::Bitrate320),
+            _ => {
+                error!("Invalid bitrate {}!", b.trim());
+                None
+            }
+        })
+        .unwrap_or(Bitrate::Bitrate160);
+
+    let cache_path = lookup("cache_path").map(String::clone).map(PathBuf::from);
+    let cache: Box<Cache + Send + Sync> = if let Some(p) = cache_path {
+        if let Ok(c) = DefaultCache::new(p) {
+            Box::new(c)
+        } else {
+            error!("Couldn't create cache, will continue without cache.");
+            Box::new(NoCache)
+        }
+    } else {
+        info!("No cache specified, will continue without cache.");
+        Box::new(NoCache)
+    };
+
+    Ok(SpotifydConfig {
+        log_path: lookup("log_path").map(|p| PathBuf::from(p)),
+        username: lookup("username").map(String::clone),
+        password: lookup("password").map(String::clone),
+        backend: lookup("backend").map(String::clone),
+        cache: cache,
+        session_config: SessionConfig {
+            bitrate: bitrate,
+            user_agent: version::version_string(),
+            onstart: lookup("onstart").map(String::clone),
+            onstop: lookup("onstop").map(String::clone),
+            device_name: lookup("device_name").map(String::clone).unwrap_or("Spotifyd".to_owned()),
+        },
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,90 @@
+extern crate getopts;
+extern crate librespot;
+extern crate env_logger;
+extern crate ini;
+extern crate xdg;
+#[macro_use]
+extern crate log;
+extern crate simple_signal;
+
+use std::process::exit;
+use std::thread;
+use std::env;
+
+use librespot::spirc::SpircManager;
+use librespot::main_helper;
+use librespot::session::Session;
+use librespot::player::Player;
+
+use simple_signal::Signal;
+
+mod config;
+
+fn usage(program: &str, opts: &getopts::Options) -> String {
+    let brief = format!("Usage: {} [options]", program);
+    format!("{}", opts.usage(&brief))
+}
+
 fn main() {
-    println!("Hello, world!");
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info,librespot=trace")
+    }
+    env_logger::init().unwrap();
+
+    let mut opts = getopts::Options::new();
+    opts.optopt("c", "config", "Path to a config file.", "CONFIG");
+    opts.optopt("", "no-daemon", "Don't detach from console.", "");
+    opts.optopt("u", "username", "Spotify user name.", "USERNAME");
+    opts.optopt("p", "password", "Spotify password.", "PASSWORD");
+    if cfg!(feature = "facebook") {
+        opts.optflag("", "facebook", "Login with a Facebook account");
+    }
+    opts.optflag("", "backends", "Available audio backends.");
+
+    let args: Vec<String> = std::env::args().collect();
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => {
+            error!("Error: {}\n{}", f.to_string(), usage(&args[0], &opts));
+            exit(1)
+        }
+    };
+
+    if matches.opt_present("backends") {
+        main_helper::find_backend(Some("?"));
+        exit(0);
+    }
+
+    let config = config::read_config().unwrap_or_else(|e| {
+        error!("Couldn't read configuration file: {:?}", e);
+        exit(1);
+    });
+
+    let cache = config.cache;
+    let backend = config.backend;
+    let session_config = config.session_config;
+    let device_name = session_config.device_name.clone();
+    let session = Session::new(session_config, cache);
+    let credentials = main_helper::get_credentials(&session, &matches);
+    session.login(credentials).unwrap();
+    let player = Player::new(session.clone(), move || {
+        main_helper::find_backend(backend.as_ref()
+            .map(AsRef::as_ref))(Some(device_name.as_ref()))
+    });
+
+    let spirc = SpircManager::new(session.clone(), player);
+    let spirc_signal = spirc.clone();
+    thread::spawn(move || spirc.run());
+
+    simple_signal::set_handler(&[Signal::Int, Signal::Term], move |signals| {
+        info!("Signal received: {:?}. Say goodbye and exit.", signals);
+        spirc_signal.send_goodbye();
+        exit(0);
+    });
+
+    loop {
+        session.poll();
+    }
+
 }
