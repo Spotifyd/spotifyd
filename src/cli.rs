b5e5d35117fa9b2b340f3f95ba71cc08d853cfde
Simon Persson <simon@flaskpost.org>
2017-06-26 12:55:55 +0200

    Fix broken command line options.

--- src/cli.rs
+++ src/cli.rs
@@ -22,13 +22,13 @@ pub fn command_line_argument_options() -> Options {
     opts.optopt("", "bitrate", "Any of 96, 160, and 320.", "DEVICE");
     opts.optopt("", "pid", "Path to PID file.", "PID-FILE");
     opts.optopt("", "device_name", "Name of this Spotify device.", "DEVICE");
+    opts.optopt("", "backend", "Audio backend.", "BACKEND");
+    opts.optopt("", "cache_path", "Path to cache location.", "PATH");
+    opts.optopt("", "onstart", "Command to run on song start.", "COMMAND");
+    opts.optopt("", "onstop", "Command to run on song stop.", "COMMAND");
     opts.optflag("v", "verbose", "Add debug information to log.");
     opts.optflag("", "no-daemon", "Don't detach from console.");
-    opts.optflag("", "backend", "Audio backend.");
     opts.optflag("", "backends", "List available audio backends.");
-    opts.optflag("", "cache_path", "Path to cache location.");
-    opts.optflag("", "onstart", "Command to run on song start.");
-    opts.optflag("", "onstop", "Command to run on song stop.");
     opts.optflag("h", "help", "Print this help text.");
     opts
 }
