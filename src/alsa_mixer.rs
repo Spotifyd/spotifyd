86facf99b9192ea440cf0092b0ce6c785717936a
Simon Persson <simon@flaskpost.me>
2018-02-14 00:11:40 +0100

    Improve volume handling.

--- src/alsa_mixer.rs
+++ src/alsa_mixer.rs
@@ -1,9 +1,31 @@
+use std::error::Error;
 use librespot::playback::mixer::{AudioFilter, Mixer};
 use alsa;
 
 pub struct AlsaMixer {
     pub device: String,
     pub mixer: String,
+}
+
+impl AlsaMixer {
+    fn set_volume_with_err(&self, volume: u16) -> Result<(), Box<Error>> {
+        let mixer = alsa::mixer::Mixer::new(&self.device, false)?;
+
+        let selem_id = alsa::mixer::SelemId::new(&*self.mixer, 0);
+        let elem = mixer.find_selem(&selem_id).ok_or("Couldn't find selem.")?;
+
+        let (min, max) = elem.get_playback_volume_range();
+
+        let volume_steps = (max - min) as f64;
+        let normalised_volume = ((volume as f64).log(65535.0) * volume_steps).floor() as i64 + min;
+
+        error!(
+            "volume={},min={},normalized={}",
+            volume, min, normalised_volume
+        );
+        elem.set_playback_volume_all(normalised_volume)?;
+        Ok(())
+    }
 }
 
 impl Mixer for AlsaMixer {
@@ -43,23 +65,10 @@ impl Mixer for AlsaMixer {
     }
 
     fn set_volume(&self, volume: u16) {
-        match alsa::mixer::Mixer::new(&self.device, false)
-            .ok()
-            .and_then(|mixer| {
-                let selem_id = alsa::mixer::SelemId::new(&*self.mixer, 0);
-                mixer.find_selem(&selem_id).and_then(|elem| {
-                    let (min, max) = elem.get_playback_volume_range();
-
-                    let volume_steps = max - min + 1;
-                    let normalised_volume: i64 =
-                        min + ((volume as i64) / (0xFFFF / volume_steps)) as i64;
-
-                    elem.set_playback_volume_all(normalised_volume).ok()
-                })
-            }) {
-            Some(_) => (),
-            _ => error!("Couldn't set volume of alsa device."),
-        };
+        match self.set_volume_with_err(volume) {
+            Ok(_) => (),
+            Err(e) => error!("Couldn't set volume: {:?}", e),
+        }
     }
 
     fn get_audio_filter(&self) -> Option<Box<AudioFilter + Send>> {
