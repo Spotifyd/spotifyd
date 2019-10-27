00c47cde510d59ad0ca15810bf26ff4cb701d7ac
Marcel Heiber <marcel.heiber@gmx.de>
2019-10-27 16:02:44 +0100

    Reimplemented logging utilizing fern

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@
 
 use daemonize::Daemonize;
 use log::{error, info, trace, LevelFilter};
-use simplelog::{ConfigBuilder, LevelPadding, SimpleLogger, TermLogger, TerminalMode};
 use structopt::StructOpt;
 use tokio_core::reactor::Core;
 
-use std::{convert::From, error::Error, panic};
+use std::panic;
 
 use crate::config::CliConfig;
 
@@ -21,34 +20,47 @@ mod process;
 mod setup;
 mod utils;
 
-fn setup_logger(is_daemon: bool, is_verbose: bool) {
-    let filter = if is_verbose {
-        LevelFilter::Trace
-    } else {
-        LevelFilter::Info
+enum LogTarget {
+    Terminal,
+    Syslog,
+}
+
+fn setup_logger(log_target: LogTarget, log_level: LevelFilter) {
+    let logger = fern::Dispatch::new().level(log_level);
+
+    let logger = match log_target {
+        LogTarget::Terminal => logger.chain(std::io::stdout()),
+        LogTarget::Syslog => {
+            let log_format = syslog::Formatter3164 {
+                facility: syslog::Facility::LOG_DAEMON,
+                hostname: None,
+                process: "spotifyd".to_owned(),
+                pid: 0,
+            };
+            logger.chain(syslog::unix(log_format).expect("Couldn't initialize logger"))
+        },
     };
 
-    if is_daemon {
-        syslog::init(syslog::Facility::LOG_DAEMON, filter, Some("Spotifyd"))
-            .expect("Couldn't initialize logger");
-    } else {
-        let logger_config = ConfigBuilder::new()
-            .set_level_padding(LevelPadding::Off)
-            .build();
-
-        TermLogger::init(filter, logger_config.clone(), TerminalMode::Mixed)
-            .map_err(Box::<dyn Error>::from)
-            .or_else(|_| SimpleLogger::init(filter, logger_config).map_err(Box::<dyn Error>::from))
-            .expect("Couldn't initialize logger");
-    }
+    logger.apply().expect("Couldn't initialize logger");
 }
 
 fn main() {
     let mut cli_config: CliConfig = CliConfig::from_args();
 
-    let is_daemon = !cli_config.no_daemon;
-    let is_verbose = cli_config.verbose;
-    setup_logger(is_daemon, is_verbose);
+    let is_deamon = !cli_config.no_daemon;
+
+    let log_target = if is_deamon {
+        LogTarget::Syslog
+     } else {
+        LogTarget::Terminal
+     };
+    let log_level = if cli_config.verbose {
+        LevelFilter::Trace
+    } else {
+        LevelFilter::Info
+    };
+    
+    setup_logger(log_target, log_level);
 
     cli_config.load_config_file_values();
     trace!("{:?}", &cli_config);
@@ -56,7 +68,7 @@ fn main() {
     // Returns the old SpotifydConfig struct used within the rest of the daemon.
     let internal_config = config::get_internal_config(cli_config);
 
-    if is_daemon {
+    if is_deamon {
         info!("Daemonizing running instance");
 
         let mut daemonize = Daemonize::new();
