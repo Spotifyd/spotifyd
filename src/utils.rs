5a3f714fd7b96f6418edb6837e8e1a6c0f9e13d9
Sven Lechner <SirWindfield@users.noreply.github.com>
2020-02-09 12:46:46 +0100

    refactor(utils): refactor get_shell to use native function calls
    This PR splits up the logic for retrieving the user's shell into separate functions for each supported OS. The Linux implementation will now use `libc` to retrieve the right shell instead of relying on the `/etc/passwd` file

--- src/utils.rs
+++ src/utils.rs
@@ -1,84 +1,102 @@
+use log::trace;
+use std::env;
+#[cfg(target_os = "macos")]
 use whoami;
 
-use std::env;
+#[cfg(target_os = "linux")]
+fn get_shell_ffi() -> Option<String> {
+    use libc::{geteuid, getpwuid_r};
+    use std::{ffi::CStr, mem, ptr};
+
+    trace!("Retrieving user shell through libc calls");
+
+    let mut result = ptr::null_mut();
+    unsafe {
+        let amt = match libc::sysconf(libc::_SC_GETPW_R_SIZE_MAX) {
+            n if n < 0 => 512 as usize,
+            n => n as usize,
+        };
+        let mut buf = Vec::with_capacity(amt);
+        let mut passwd: libc::passwd = mem::zeroed();
+
+        match getpwuid_r(
+            geteuid(),
+            &mut passwd,
+            buf.as_mut_ptr(),
+            buf.capacity() as libc::size_t,
+            &mut result,
+        ) {
+            0 if !result.is_null() => {
+                let ptr = passwd.pw_shell as *const _;
+                let shell = CStr::from_ptr(ptr)
+                    .to_str()
+                    .expect("Failed to retrieve shell")
+                    .to_owned();
+                Some(shell)
+            }
+            _ => None,
+        }
+    }
+}
+
+// If the SHELL environment variable is not set and on we're on macOS,
+// query the Directory Service command line utility (dscl) for the user's shell,
+// as macOS does not use the /etc/passwd file
+#[cfg(target_os = "macos")]
+fn get_shell_ffi() -> Option<String> {
+    use std::process::Command;
+
+    trace!("Retrieving user shell through Directory Discovery Services");
+
+    let username = whoami::username();
+    let output = Command::new("dscl")
+        .args(&[".", "-read", &format!("/Users/{}", username), "UserShell"])
+        .output()
+        .ok()?;
+
+    if output.status.success() {
+        let stdout = std::str::from_utf8(&output.stdout).ok()?;
+        // The output of the dscl command should be:
+        // "UserShell: /path/to/shell"
+        if stdout.starts_with("UserShell: ") {
+            let shell = stdout.split_whitespace().nth(1)?;
+            return Some(shell.to_string());
+        }
+    }
+
+    None
+}
 
 pub(crate) fn get_shell() -> Option<String> {
-    // First look for the user's preferred shell using the SHELL environment
-    // variable...
-    if let Ok(shell) = env::var("SHELL") {
-        log::trace!("Found shell {:?} using SHELL environment variable.", shell);
-        return Some(shell);
-    }
+    let shell = env::var("SHELL").ok().or_else(get_shell_ffi);
+    trace!("Found user shell: {:?}", &shell);
 
-    // If the SHELL environment variable is not set and we're on linux or one of the
-    // BSDs, try to obtain the default shell from `/etc/passwd`...
-    #[cfg(not(target_os = "macos"))]
-    {
-        use std::{
-            fs::File,
-            io::{self, BufRead},
-        };
-
-        let username = whoami::username();
-
-        let file = File::open("/etc/passwd").ok()?;
-        let reader = io::BufReader::new(file);
-        // Each line of `/etc/passwd` describes a single user and contains seven
-        // colon-separated fields: "name:password:UID:GID:GECOS:directory:shell"
-        for line in reader.lines() {
-            let line = line.ok()?;
-            let mut iter = line.split(':');
-            if let Some(user) = iter.nth(0) {
-                if user == username {
-                    let shell = iter.nth(5)?;
-                    log::trace!("Found shell {:?} using /etc/passwd.", shell);
-                    return Some(shell.into());
-                }
-            }
-        }
-    }
-
-    // If the SHELL environment variable is not set and on we're on macOS,
-    // query the Directory Service command line utility (dscl) for the user's shell,
-    // as macOS does not use the /etc/passwd file...
-    #[cfg(target_os = "macos")]
-    {
-        use std::process::Command;
-
-        let username = whoami::username();
-        let output = Command::new("dscl")
-            .args(&[".", "-read", &format!("/Users/{}", username), "UserShell"])
-            .output()
-            .ok()?;
-        if output.status.success() {
-            let stdout = std::str::from_utf8(&output.stdout).ok()?;
-            // The output of this dscl command should be:
-            // "UserShell: /path/to/shell"
-            if stdout.starts_with("UserShell: ") {
-                let shell = stdout.split_whitespace().nth(1)?;
-                log::trace!("Found shell {:?} using dscl command.", shell);
-                return Some(shell.to_string());
-            }
-        }
-    }
-    None
+    shell
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn init_logger() {
+        let _ = env_logger::builder().is_test(true).try_init();
+    }
+
     #[test]
-    fn it_works() {
-        env::set_var("RUST_LOG", "spotifyd=trace");
+    fn test_envvar_discovery() {
+        init_logger();
 
-        env_logger::init();
+        env::set_var("SHELL", "fantasy_shell");
 
-        let _ = get_shell().unwrap();
+        let shell = get_shell().unwrap();
+        assert_eq!(shell, "fantasy_shell");
+    }
 
-        if env::var("SHELL").is_ok() {
-            env::remove_var("SHELL");
-            let _ = get_shell().unwrap();
-        }
+    #[test]
+    fn test_ffi_discovery() {
+        init_logger();
+
+        let shell = get_shell_ffi();
+        assert_eq!(shell.is_some(), true);
     }
 }
