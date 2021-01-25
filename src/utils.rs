a352eab53eef0ecafe1ddb07a43ff2b3ab54f123
MikaelUrankar <mikael.urankar@gmail.com>
2021-01-25 12:31:42 +0100

    Fix build on FreeBSD

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@ use std::env;
 #[cfg(target_os = "macos")]
 use whoami;
 
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "freebsd", target_os = "linux"))]
 fn get_shell_ffi() -> Option<String> {
     use libc::{geteuid, getpwuid_r};
     use std::{ffi::CStr, mem, ptr};
