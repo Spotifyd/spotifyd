12309ba4e1d8f06a157c27260d920d85c8b9d4ab
eladyn <eladyn@protonmail.com>
2022-05-29 01:05:37 +0200

    reduce requested token scopes

--- src/dbus_mpris.rs
+++ src/dbus_mpris.rs
@@ -45,12 +45,8 @@ pub struct DbusServer {
 }
 
 const CLIENT_ID: &str = "2c1ea588dfbc4a989e2426f8385297c3";
-const SCOPE: &str = "user-read-playback-state,user-read-private,\
-                     user-read-email,playlist-read-private,user-library-read,user-library-modify,\
-                     user-top-read,playlist-read-collaborative,playlist-modify-public,\
-                     playlist-modify-private,user-follow-read,user-follow-modify,\
-                     user-read-currently-playing,user-modify-playback-state,\
-                     user-read-recently-played";
+const SCOPE: &str =
+    "user-read-playback-state,user-modify-playback-state,user-read-currently-playing";
 
 impl DbusServer {
     pub fn new(
